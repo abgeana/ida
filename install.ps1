69580e16ea6d20c7d5bc14be78693e189bb12fdc
Alexandru Geana <alex@alegen.net>
2018-11-18 10:28:10 +0100

    adding the easy install script for plugins

---
+++ install.ps1
@@ -0,0 +1,18 @@
+# first enable execution of scripts with the following command
+# set-executionpolicy remotesigned
+
+$ida_plugins_dir = "$env:APPDATA\Hex-Rays\IDA Pro\plugins"
+$plugin_dirs = @(
+    "IDASkins\plugins"
+)
+
+# clean the existing plugins folder
+if (Test-Path -Path $ida_plugins_dir) {
+    Remove-Item -Recurse -Path "$ida_plugins_dir" | Out-Null
+}
+New-Item -ItemType directory -Path "$ida_plugins_dir" | Out-Null
+
+# copy the plugins
+foreach ($p in $plugin_dirs) {
+    Copy-Item -Recurse ".\$p\*" -Destination "$ida_plugins_dir"
+}
